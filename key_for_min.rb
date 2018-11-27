a3025c4873f3093309ce5f8a0716982f9e66a356
Tara Sabella <tarasabella@gmail.com>
2018-11-27 15:43:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,3 +8,15 @@ end
 name_hash.collect do |key|
  name_hash.first
 end
+
+def key_for_min_value(name_hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
+end
